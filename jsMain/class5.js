f10662aa9120dd18a86778b76cf73ee93706a0f7
Mosarrof-Coding <tinaberry112188@gmail.com>
2023-09-20 20:31:28 +0600

    arr_obj_return

--- jsMain/class5.js
+++ jsMain/class5.js
@@ -1,4 +1,4 @@
-// functions..........for loop continous by function
+// for loop: continous by function
 function forLoop(countStart, countEnd){
     for (let counter = countStart; counter <= countEnd; counter++) {
         console.log("forLoop:", counter);
@@ -8,12 +8,11 @@ function forLoop(countStart, countEnd){
 forLoop(89, 90);
 forLoop(79, 80);
 
-// userName generator
-
-function userName(){
-    console.log("user is Jamal");
+// profile generator
+function profile(name, age, maritul_status){
+    console.log(`profile: User name is ${name} age ${age} and ${maritul_status}`);
 }
-userName();
+profile("Jamal", 37, "unmarried.");
 
 // random number between 1 to 10
 function randomNumber(){
@@ -23,10 +22,38 @@ function randomNumber(){
 randomNumber();
 randomNumber();
 
-// return -> basically return executes the function rather not called yet. if call it showes the result.
+// return -> basically return executes the codes rather not called yet. if call it showes the result.
 function add(num1, num2){
     // console.log(num1 + num2);
     return num1 + num2;
 }
 let rslt = add(20, 30);
-console.log(rslt);
+console.log("return:", rslt);
+
+// object......
+let person = {
+    name : "doddle",
+    age : 34,
+    country : "bangladesh",
+    salary : 34000,
+    teaching : function (){
+        console.log("institue: asian university");
+    },
+    gratuity : function (month, quantity){
+        return month * quantity;
+    }
+}
+console.log("country:", person.country);
+person.teaching();
+let totals = person.gratuity(720, 3000);
+console.log("gratuity:", totals);
+
+// Arrays
+let datum = ["mango", undefined, 34, "berry", false, 20000, "tomato", null, BigInt];
+console.log(datum);
+datum.push("wheel");
+console.log(datum);
+datum.shift();
+console.log(datum);
+datum.pop();
+console.log(datum);
